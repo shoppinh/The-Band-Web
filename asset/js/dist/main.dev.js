92c4ca7f3a17aed0412e7fe747b2c2fec9b314d3
Kienneik <mactrungkien2000@gmail.com>
2021-02-17 22:26:15 +0700

    fix the menu on phone mode

---
+++ asset/js/dist/main.dev.js
@@ -0,0 +1,25 @@
+"use strict";
+
+var header = document.getElementById('header');
+var mobile_btn = document.querySelector('#header .mobile-menu-btn');
+var currentHeight = header.clientHeight;
+
+mobile_btn.onclick = function () {
+  var isClosed = header.clientHeight === currentHeight;
+
+  if (isClosed) {
+    header.style.height = 'auto';
+  } else {
+    header.style.height = "".concat(currentHeight, "px");
+  }
+};
+
+var mobile_refs = document.querySelectorAll('#nav li a[href*="#"]');
+
+for (var i = 0; i < mobile_refs.length; i++) {
+  var menuItem = mobile_refs[i];
+
+  menuItem.onclick = function () {
+    header.style.height = "".concat(currentHeight, "px");
+  };
+}
--- asset/js/main.js
+++ asset/js/main.js
@@ -1,4 +1,21 @@
-var header = document.getElementById("header")
-header.onclick = function showMessage() {
-    console.log('hello guys')
+var header = document.getElementById('header');
+
+var mobile_btn = document.querySelector('#header .mobile-menu-btn');
+var currentHeight = header.clientHeight;
+mobile_btn.onclick = function () {
+    var isClosed = header.clientHeight=== currentHeight;
+    if (isClosed) {
+        header.style.height = 'auto';
+
+    } else {
+        header.style.height = `${currentHeight}px`;
+    }
+}
+
+var mobile_refs = document.querySelectorAll('#nav li a[href*="#"]');
+for (var i=0; i<mobile_refs.length; i++) {
+    var menuItem = mobile_refs[i];
+    menuItem.onclick = function () {
+         header.style.height = `${currentHeight}px`;
+    }
 }
